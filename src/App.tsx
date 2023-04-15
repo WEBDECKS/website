689e9a3b96490f2debbf6c50d1f766155f3e936a
Stefan <blockchain.crypto.stefan@gmail.com>
2023-04-15 23:07:06 +0100

    Updated holding page

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,11 @@ function App() {
       <div className="flex flex-col h-screen justify-center items-center">
         <h1 className="text-3xl font-bold text-gray-800 mb-6">WEBDECKS</h1>
         <p className="text-gray-600 mb-6 text-center max-w-xs">
-          Welcome to the absurdly long named Web3 and Blockchain Development
-          Mastermind Group!
+          Welcome to the absurdly long-named Web3 and Blockchain Development
+          Exploration, Collaboration, and Knowledge-Sharing Mastermind Group!
         </p>
         <a
-          href="https://github.com/"
+          href="https://github.com/WEBDECKS"
           className="bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
         >
           <i className="fab fa-github mr-2"></i> Visit our Github
